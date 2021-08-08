d2d031dd1a676a640c4c0231c55ec6d59251df8f
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2021-08-07 20:54:53 -0700

    update

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+
+const app = express();
+
+const personsRouter = require('./controllers/blog');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+
+const mongoUrl = config.DB_URI;
+
+logger.info('connecting to mongodb');
+
+mongoose
+    .connect(mongoUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    })
+    .then((response) => {
+        logger.info('connected to mongodb');
+    })
+    .catch((error) => {
+        logger.error(error);
+    });
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', personsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ models/blog.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        minlength: 12,
+        required: true,
+    },
+    author: {
+        type: String,
+        minlength: 8,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    likes: {
+        type: Number,
+        required: true,
+    },
+});
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;

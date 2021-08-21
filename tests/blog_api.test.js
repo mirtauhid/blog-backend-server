8f87f55ebd00d61b0121aeada62709c9bf28c1e1
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2021-08-21 14:05:26 -0700

    supertest added

---
+++ tests/blog_api.test.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const Blog = require('../models/blog');
+
+const api = supertest(app);
+
+const blogs = [
+    {
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7,
+    },
+    {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+    },
+];
+
+beforeEach(async () => {
+    await Blog.deleteMany({});
+    let blogObject = new Blog(blogs[0]);
+    await blogObject.save();
+    blogObject = new Blog(blogs[1]);
+    await blogObject.save();
+});
+
+test('all blogs returned', async () => {
+    const response = await api.get('/api/blogs');
+
+    expect(response.body).toHaveLength(blogs.length);
+});
+
+test('should return blogs in json format', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+}, 100000);
+
+afterAll(() => {
+    mongoose.connection.close();
+});
--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { DB_URI } = process.env;
+const DB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_DB_URI : process.env.DB_URI;
 const { PORT } = process.env;
 
 module.exports = {
